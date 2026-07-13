f718f00b4188b0b512049accd497fc8d66a55924
J. Ignacio Montalbán <montardi.juanignacio@gmail.com>
2026-07-13 15:13:26 +0200

    Limitada la generación de frames a 60FPS

--- main.py
+++ main.py
@@ -3,18 +3,34 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 def main():
+    # Iniciacion de pygame
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Variables para 60 FPS
+    reloj: pygame = pygame.time.Clock()
+    dt: float = 0.0
+
+    # Tamaño de la ventana a usar
+    screen: pygame = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
     while True:
+        # Funcion de bootdev para comprobar el funcionamiento correcto
         log_state()
+
+        # Event queue que revisamos en cada iteracion
         for event in pygame.event.get():
+            # Evento para salir del loop al cerrar la ventana
             if event.type == pygame.QUIT:
                 return
+        # Color de la pantalla 
         screen.fill("black")
+
+        # Refresca la pantalla
         pygame.display.flip()
+
+        # DeltaTime indica el tiempo entre tick para 60FPS, en milisegundos. 
+        dt = reloj.tick(60) / 1000 
 
 
 if __name__ == "__main__":
