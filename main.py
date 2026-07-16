9ce4b0bad05edaf352bf76e01a94983bbdc6f5dd
J. Ignacio Montalbán <montardi.juanignacio@gmail.com>
2026-07-16 17:33:15 +0200

    Creadas las clases Asteroid y AsteroidField

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -7,6 +9,8 @@ def main():
     # Iniciacion de pygame
     pygame.init()
 
+    # Tamaño de la ventana a usar
+    screen: pygame = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # Variables para 60 FPS
     reloj: pygame = pygame.time.Clock()
     dt: float = 0.0
@@ -14,11 +18,14 @@ def main():
     # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    # Tamaño de la ventana a usar
-    screen: pygame = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Player.containers = (updatable, drawable)
-    player1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    astrofield = AsteroidField()
 
     # Game loop
     while True:
