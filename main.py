cb1552b3534a21dc0068eee3740fdc0a2b7427d4
J Ignacio M <montardi.juanignacio@gmail.com>
2026-07-19 17:10:24 +0200

    Los disparos ahora matan los asteroides

--- main.py
+++ main.py
@@ -52,6 +52,12 @@ def main():
                 print("Game over")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
         # Color de la pantalla
         screen.fill("black")
         # Dibula a los objetos del grupo drawable en la pantalla
